a61b60f431efb94de224905c27a4b47b1631b861
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-06-29 16:58:05 +0900

    ログイン後のはじめの画面の大枠を実装

--- android/src/src/com/tech_tec/qiitarian/activity/HomeActivity.java
+++ android/src/src/com/tech_tec/qiitarian/activity/HomeActivity.java
@@ -1,7 +1,16 @@
 package com.tech_tec.qiitarian.activity;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-public class HomeActivity extends FragmentActivity {
+import com.tech_tec.qiitarian.R;
 
+public class HomeActivity extends FragmentActivity {
+    
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        setContentView(R.layout.activity_home);
+    }
+    
 }
