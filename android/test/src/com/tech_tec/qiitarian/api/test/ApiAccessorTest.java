48c0a1ca02880ed9b46960600f7417fa3bc3abe5
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-06-29 12:24:36 +0900

    APIアクセスクラスのテストを実装

---
+++ android/test/src/com/tech_tec/qiitarian/api/test/ApiAccessorTest.java
@@ -0,0 +1,55 @@
+package com.tech_tec.qiitarian.api.test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.tech_tec.qiitarian.api.ApiAccessor;
+import com.tech_tec.qiitarian.api.ApiAccessor.JSONObjectFactory;
+import com.tech_tec.qiitarian.model.AuthInfo;
+import com.tech_tec.qiitarian.model.LoginService;
+
+public class ApiAccessorTest extends TestCase {
+    
+    public void testログイン成功() throws JSONException, ClientProtocolException, IOException {
+        String username = "shoma2da";
+        String password = "hogehoge";
+        String token = "abcde";
+        
+        String url = String.format("https://qiita.com/api/v1/auth?url_name=%s&password=%s", username, password);
+        JSONObject json = new JSONObject(String.format("{ \"token\" : \"%s\" }", token));
+        
+        JSONObjectFactory jsonObjectFactory = mock(JSONObjectFactory.class);
+        when(jsonObjectFactory.create(url)).thenReturn(json);
+        ApiAccessor accessor = new ApiAccessor(jsonObjectFactory);
+        
+        AuthInfo info = accessor.auth(username, password, LoginService.TWITTER);
+        assertEquals(info.getUsername(), username);
+        assertEquals(info.getToken(), token);
+        assertEquals(info.getService(), LoginService.TWITTER);
+    }
+    
+    public void testログイン失敗() throws ClientProtocolException, IOException, JSONException {
+        String username = "shoma2da";
+        String password = "hogehoge";
+        
+        String url = String.format("https://qiita.com/api/v1/auth?url_name=%s&password=%s", username, password);
+        JSONObject json = new JSONObject("{}");
+        
+        JSONObjectFactory jsonObjectFactory = mock(JSONObjectFactory.class);
+        when(jsonObjectFactory.create(url)).thenReturn(json);
+        ApiAccessor accessor = new ApiAccessor(jsonObjectFactory);
+        
+        AuthInfo info = accessor.auth(username, password, LoginService.TWITTER);
+        assertEquals(info.getUsername(), username);
+        assertNull(info.getToken());
+        assertEquals(info.getService(), LoginService.TWITTER);
+    }
+}
