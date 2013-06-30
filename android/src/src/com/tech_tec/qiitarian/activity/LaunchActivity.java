5d61fc64155f9ed1bc86c7af8cf0e2a64463ac51
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-06-30 12:12:58 +0900

    起動画面を追加し、ログイン画面への遷移とホーム画面への遷移をだし分けられるように大枠を実装

---
+++ android/src/src/com/tech_tec/qiitarian/activity/LaunchActivity.java
@@ -0,0 +1,33 @@
+package com.tech_tec.qiitarian.activity;
+
+import javax.inject.Inject;
+
+import com.tech_tec.qiitarian.R;
+import com.tech_tec.qiitarian.model.AuthInfo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LaunchActivity extends Activity {
+    
+    @Inject AuthInfo mAuthInfo;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        
+        if (hasAuthInfo()) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+    
+    private boolean hasAuthInfo() {
+        return mAuthInfo != null && mAuthInfo.getToken() != null &&  mAuthInfo.getToken().length() != 0;
+    }
+}
