e3db9db0e537ce76bcd1adaa8788b045618f654a
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-07 23:59:37 +0900

    InputStream取得のテストと実装

--- android/src/src/com/tech_tec/qiitarian/util/QiitaJsonHttpClient.java
+++ android/src/src/com/tech_tec/qiitarian/util/QiitaJsonHttpClient.java
@@ -1,7 +1,9 @@
 package com.tech_tec.qiitarian.util;
 
 import java.io.IOException;
+import java.io.InputStream;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpUriRequest;
@@ -15,9 +17,13 @@ public class QiitaJsonHttpClient {
         mRealClieant = new DefaultHttpClient();
     }
     
-    public void execute(HttpUriRequest request) throws ClientProtocolException, IOException {
-        mRealClieant.execute(request);
+    public InputStream execute(HttpUriRequest request) throws ClientProtocolException, IOException {
+        HttpResponse response = mRealClieant.execute(request);
+        InputStream input = response.getEntity().getContent();
+        
         mRealClieant.getConnectionManager().shutdown();
+        
+        return input;
     }
     
 }
--- android/test_jvm/test/com/tech_tec/qiitarian/util/QiitaJsonHttpClientTest.java
+++ android/test_jvm/test/com/tech_tec/qiitarian/util/QiitaJsonHttpClientTest.java
@@ -5,7 +5,10 @@ import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
 import java.io.IOException;
+import java.io.InputStream;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -19,11 +22,19 @@ public class QiitaJsonHttpClientTest {
     private QiitaJsonHttpClient mClient;
     
     @Before
-    public void setUp() {
+    public void setUp() throws IllegalStateException, IOException {
         mClient = new QiitaJsonHttpClient();
         mClient.mRealClieant = mock(HttpClient.class);
+        
         ClientConnectionManager mockConnectionManager = mock(ClientConnectionManager.class);
+        HttpResponse mockResponse = mock(HttpResponse.class);
+        HttpEntity mockEntity = mock(HttpEntity.class);
+        InputStream mockInput = mock(InputStream.class);
+        
         when(mClient.mRealClieant.getConnectionManager()).thenReturn(mockConnectionManager);
+        when(mockEntity.getContent()).thenReturn(mockInput);
+        when(mockResponse.getEntity()).thenReturn(mockEntity);
+        when(mClient.mRealClieant.execute((HttpUriRequest)anyObject())).thenReturn(mockResponse);
     }
     
     @Test
@@ -60,5 +71,16 @@ public class QiitaJsonHttpClientTest {
     public void メンバのクライアントオブジェクトでnullポインターが出ないようにする() throws ClientProtocolException, IOException {
         QiitaJsonHttpClient client = new QiitaJsonHttpClient();
         assertNotNull(client.mRealClieant);
+    }
+    
+    @Test
+    public void レスポンスのInputStreamを取得する() throws IllegalStateException, IOException {
+        //準備
+        HttpUriRequest mockRequest = mock(HttpUriRequest.class);
+        InputStream mockInput = mock(InputStream.class);
+        when(mClient.mRealClieant.execute(mockRequest).getEntity().getContent()).thenReturn(mockInput);
+        
+        InputStream realInput = mClient.execute(mockRequest);
+        assertEquals(mockInput, realInput);
     }
 }
