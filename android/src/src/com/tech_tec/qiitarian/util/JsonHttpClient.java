484091bc51f9d6b703cbbbd495418f2acaf6dc9e
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-07 13:23:53 +0900

    httpclientの初期オブジェクトを用意

--- android/src/src/com/tech_tec/qiitarian/util/JsonHttpClient.java
+++ android/src/src/com/tech_tec/qiitarian/util/JsonHttpClient.java
@@ -5,10 +5,15 @@ import java.io.IOException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 public class JsonHttpClient {
     
     HttpClient mRealClieant;
+    
+    public JsonHttpClient() {
+        mRealClieant = new DefaultHttpClient();
+    }
     
     public void execute(HttpUriRequest request) throws ClientProtocolException, IOException {
         mRealClieant.execute(request);
