a19f6612b8ab37af4232223654be28e641956aa6
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-15 08:25:34 +0900

    InputStreamからのパースに対応

--- android/src/src/com/tech_tec/qiitarian/model/items/parser/ItemsParser.java
+++ android/src/src/com/tech_tec/qiitarian/model/items/parser/ItemsParser.java
@@ -1,11 +1,30 @@
 package com.tech_tec.qiitarian.model.items.parser;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 import org.json.JSONArray;
+import org.json.JSONException;
 
 import com.tech_tec.qiitarian.model.items.Item;
 import com.tech_tec.qiitarian.model.items.Items;
 
 public class ItemsParser {
+    
+    public Items parse(InputStream input) throws JSONException {
+        String content = inputToString(input);
+        JSONArray jsonArray = new JSONArray(content);
+        return parse(jsonArray);
+    }
+    
+    String inputToString(InputStream input) {
+        StringBuilder builder = new StringBuilder();
+        Scanner scanner = new Scanner(input);
+        while(scanner.hasNextLine()) {
+            builder.append(scanner.nextLine());
+        }
+        return builder.toString();
+    }
     
     public Items parse(JSONArray jsonArray) {
         Items items = new Items();
--- android/test_jvm/test/com/tech_tec/qiitarian/model/items/parser/ItemsParserTest.java
+++ android/test_jvm/test/com/tech_tec/qiitarian/model/items/parser/ItemsParserTest.java
@@ -2,6 +2,9 @@ package com.tech_tec.qiitarian.model.items.parser;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.junit.Test;
@@ -29,4 +32,15 @@ public class ItemsParserTest {
         assertEquals(3, items.size());
     }
     
+    @Test
+    public void InputStreamから空のItemsを生成() throws JSONException {
+        InputStream input = new ByteArrayInputStream("[]".getBytes());
+        
+        ItemsParser parser = new ItemsParser();
+        Items items = parser.parse(input);
+        
+        assertNotNull(items);
+        assertTrue(items.isEmpty());
+        assertEquals(0, items.size());
+    }
 }
