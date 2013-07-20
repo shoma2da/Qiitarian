140d17c0555e10beb16eb2e2cc62532870bb1038
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-20 09:58:18 +0900

    ログ用クラスを追加

---
+++ android/src/src/com/tech_tec/qiitarian/QiitarianLog.java
@@ -0,0 +1,13 @@
+package com.tech_tec.qiitarian;
+
+import android.util.Log;
+
+public class QiitarianLog {
+    
+    public static final String TAG = "qiitarian";
+    
+    public static void d(String message) {
+        Log.d(TAG, message);
+    }
+    
+}
