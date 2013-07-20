b131daaec571ff20ac8e728991032d087a920fba
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-20 09:46:24 +0900

    デバッグログ

--- android/src/src/com/tech_tec/qiitarian/model/items/http/ItemsClient.java
+++ android/src/src/com/tech_tec/qiitarian/model/items/http/ItemsClient.java
@@ -9,11 +9,16 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import android.util.Log;
+
 public class ItemsClient {
     
     public HttpResponseWrapper execute() throws ClientProtocolException, IOException {
         HttpClient client = createHttpClient();
-        HttpResponse response = client.execute(createRequest());
+        HttpUriRequest request = createRequest();
+                
+        Log.d("qiitarian", "access to " + request.getURI());
+        HttpResponse response = client.execute(request);
         
         return new HttpResponseWrapper(response);
     }
