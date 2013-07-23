a0f5fa54022830118c8bc918ad7ecf065366e8a6
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-23 17:03:43 +0900

    投稿詳細取得周りのリファクタリング

--- android/src/src/com/tech_tec/qiitarian/fragment/content/detail/http/DetailClient.java
+++ android/src/src/com/tech_tec/qiitarian/fragment/content/detail/http/DetailClient.java
@@ -1,39 +1,24 @@
 package com.tech_tec.qiitarian.fragment.content.detail.http;
 
-import java.io.IOException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.DefaultHttpClient;
 
-import com.tech_tec.qiitarian.QiitarianLog;
 import com.tech_tec.qiitarian.model.auth.AuthInfo;
+import com.tech_tec.qiitarian.model.http.ClientBase;
 import com.tech_tec.qiitarian.model.items.Uuid;
-import com.tech_tec.qiitarian.model.items.http.HttpResponseWrapper;
 
-public class DetailClient {
+public class DetailClient extends ClientBase {
     
-    //TODO ItemsClient, AuthInfoClientと重複しまくり
-    public HttpResponseWrapper execute(Uuid uuid, AuthInfo info) throws ClientProtocolException, IOException {
-        HttpClient client = createHttpClient();
-        HttpUriRequest request = createRequest(uuid, info);
-                
-        QiitarianLog.d("access to " + request.getURI());
-        HttpResponse response = client.execute(request);
-        QiitarianLog.d("finish access");
-        
-        return new HttpResponseWrapper(response);
+    private Uuid mUuid;
+    private AuthInfo mAuthInfo;
+    
+    public DetailClient(Uuid uuid, AuthInfo info) {
+        mUuid = uuid;
+        mAuthInfo = info;
     }
     
-    HttpClient createHttpClient() {
-        return new DefaultHttpClient();
-    }
-    
-    HttpUriRequest createRequest(Uuid uuid, AuthInfo authInfo) {
-        String url = String.format("https://qiita.com/api/v1/items/%s?token=", uuid.toString(), authInfo.getTokenStr());
+    protected HttpUriRequest createRequest() {
+        String url = String.format("https://qiita.com/api/v1/items/%s?token=", mUuid.toString(), mAuthInfo.getTokenStr());
         return new HttpGet(url);
     }
 
