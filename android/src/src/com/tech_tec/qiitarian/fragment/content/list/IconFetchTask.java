95dc95e3036016e0ed9274c7855cad9054cdcb34
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-25 07:19:19 +0900

    アイコン取得を高速化

--- android/src/src/com/tech_tec/qiitarian/fragment/content/list/IconFetchTask.java
+++ android/src/src/com/tech_tec/qiitarian/fragment/content/list/IconFetchTask.java
@@ -3,22 +3,22 @@ package com.tech_tec.qiitarian.fragment.content.list;
 import java.io.IOException;
 
 import android.graphics.Bitmap;
-import android.os.AsyncTask;
+import android.os.Handler;
 import android.widget.ImageView;
 
 import com.tech_tec.qiitarian.model.items.Item;
 
-public class IconFetchTask extends AsyncTask<Void, Void, Bitmap> {
+public class IconFetchTask {
     
     private ImageView mImageView;
     private Item mItem;
+    private Handler mHandler = new Handler();
     
     public IconFetchTask(ImageView imageView, Item item) {
         mImageView = imageView;
         mItem = item;
     }
     
-    @Override
     protected Bitmap doInBackground(Void... params) {
         try {
             return mItem.fetchIconImg();
@@ -28,14 +28,34 @@ public class IconFetchTask extends AsyncTask<Void, Void, Bitmap> {
         }
     }
     
-    @Override
     protected void onPostExecute(Bitmap result) {
-        super.onPostExecute(result);
-        
         if (result == null) {
             return;
         }
         mImageView.setImageBitmap(result);
     }
-
+    
+    public void execute() {
+        new Thread(createRunnable()).start();
+    }
+    
+    private Runnable createRunnable() {
+        return new Runnable() {
+            @Override
+            public void run() {
+                Bitmap icon = doInBackground();
+                mHandler.post(createRunnableForUi(icon));
+            }
+        };
+    }
+    
+    private Runnable createRunnableForUi(final Bitmap icon) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                onPostExecute(icon);
+            }
+        };
+    }
+    
 }
