164f93e85c58c76d68474f419bde7f746bd368ed
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-25 17:02:16 +0900

    UuidとUserをDetailに追加

--- android/src/src/com/tech_tec/qiitarian/model/detail/Detail.java
+++ android/src/src/com/tech_tec/qiitarian/model/detail/Detail.java
@@ -1,13 +1,16 @@
 package com.tech_tec.qiitarian.model.detail;
 
 import com.tech_tec.qiitarian.model.items.User;
+import com.tech_tec.qiitarian.model.items.Uuid;
 
 public class Detail {
     
     private User mUser;
+    private Uuid mUuid;
     
-    public Detail(User user) {
-        
+    public Detail(Uuid uuid, User user) {
+        mUuid = uuid;
+        mUser = user;
     }
     
 }
--- android/src/src/com/tech_tec/qiitarian/model/detail/parser/DetailParser.java
+++ android/src/src/com/tech_tec/qiitarian/model/detail/parser/DetailParser.java
@@ -1,13 +1,23 @@
 package com.tech_tec.qiitarian.model.detail.parser;
 
+import java.net.MalformedURLException;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.tech_tec.qiitarian.model.detail.Detail;
+import com.tech_tec.qiitarian.model.items.User;
+import com.tech_tec.qiitarian.model.items.Uuid;
+import com.tech_tec.qiitarian.model.items.parser.UserParser;
+import com.tech_tec.qiitarian.model.items.parser.UuidParser;
 
 public class DetailParser {
     
-    public Detail parse(JSONObject object) {
-        return null;
+    public Detail parse(JSONObject object) throws MalformedURLException, JSONException {
+        Uuid uuid = new UuidParser().parse(object);
+        User user = new UserParser().parse(object.getJSONObject("user"));
+        
+        return new Detail(uuid, user);
     }
     
 }
