995f32f0358f8b81e1e1a890d485072fc12ecf86
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-28 12:20:49 +0900

    過去の投稿を取得できるように実装

--- android/src/src/com/tech_tec/qiitarian/fragment/content/list/FetchMoreContentOnScrollListener.java
+++ android/src/src/com/tech_tec/qiitarian/fragment/content/list/FetchMoreContentOnScrollListener.java
@@ -1,21 +1,35 @@
 package com.tech_tec.qiitarian.fragment.content.list;
 
+import java.util.Iterator;
+
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
-import android.widget.ListAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.tech_tec.qiitarian.R;
+import com.tech_tec.qiitarian.model.items.Item;
+import com.tech_tec.qiitarian.model.items.Items;
+import com.tech_tec.qiitarian.task.list.FetchItemsAsyncTask;
+import com.tech_tec.qiitarian.task.list.FetchItemsAsyncTask.Callback;
+import com.tech_tec.qiitarian.task.list.FetchItemsAsyncTask.UiCallback;
 
 public class FetchMoreContentOnScrollListener implements OnScrollListener {
     
+    static final int MAX_PAGE = 15;
+    
     private ListView mListView;
-    private ListAdapter mAdapter;
+    private ArrayAdapter<Item> mAdapter;
     private LayoutInflater mInflater;
     
-    public FetchMoreContentOnScrollListener(ListView listView, LayoutInflater inflater, ListAdapter adapter) {
+    private View mFooter;
+    private Integer mCurrentPage = 1;
+    private AsyncTask<Integer, Void, Items> mAsyncTask;
+    
+    public FetchMoreContentOnScrollListener(ListView listView, LayoutInflater inflater, ArrayAdapter<Item> adapter) {
         mListView = listView;
         mInflater = inflater;
         mAdapter = adapter;
@@ -23,14 +37,49 @@ public class FetchMoreContentOnScrollListener implements OnScrollListener {
 
     @Override
     public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-        if (mAdapter.getCount() == 0 || mListView.getFooterViewsCount() != 0) {
+        if (mCurrentPage == MAX_PAGE) {
+            return;
+        }
+        if (mAdapter.getCount() == 0) {
+            return;
+        }
+        if (firstVisibleItem + visibleItemCount != totalItemCount) {//リストの最後尾でないなら
+            return;
+        }
+        if (mAsyncTask != null) {
             return;
         }
         
-        if (firstVisibleItem + visibleItemCount == totalItemCount) {//リストの最後尾を表示したら
-            View footer = mInflater.inflate(R.layout.layout_list_footer, null);
-            mListView.addFooterView(footer);
-        }
+        Callback callback = new Callback() {
+            @Override
+            public void onSuccess(Items items) {
+                Iterator<Item> iterator = items.getItemIterator();
+                while (iterator.hasNext()) {
+                    mAdapter.add(iterator.next());
+                }
+            }
+            @Override
+            public void onError() {
+            }
+            @Override
+            public void onEmptySuccess() {
+            }
+        };
+        UiCallback uiCallback = new UiCallback() {
+            @Override
+            public void onPreExecute() {
+                if (mListView.getFooterViewsCount() == 0) {
+                    mFooter = mInflater.inflate(R.layout.layout_list_footer, null);
+                    mListView.addFooterView(mFooter);
+                }
+            }
+            @Override
+            public void onPostExecute() {
+                mListView.removeFooterView(mFooter);
+                mAsyncTask = null;
+            }
+        };
+        mAsyncTask = new FetchItemsAsyncTask(callback, uiCallback).execute(++mCurrentPage); //PullToUpdateで更新するのは常に最新の情報
     }
 
     @Override
