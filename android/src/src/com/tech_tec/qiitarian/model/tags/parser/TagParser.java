f9e8e31cc57c3bf34389330a156070162eb6074c
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-30 08:12:03 +0900

    Tagの追加を実装

---
+++ android/src/src/com/tech_tec/qiitarian/model/tags/parser/TagParser.java
@@ -0,0 +1,13 @@
+package com.tech_tec.qiitarian.model.tags.parser;
+
+import org.json.JSONObject;
+
+import com.tech_tec.qiitarian.model.tags.Tag;
+
+public class TagParser {
+    
+    public Tag parse(JSONObject object) {
+        return new Tag();
+    }
+    
+}
--- android/src/src/com/tech_tec/qiitarian/model/tags/parser/TagsParser.java
+++ android/src/src/com/tech_tec/qiitarian/model/tags/parser/TagsParser.java
@@ -1,16 +1,27 @@
 package com.tech_tec.qiitarian.model.tags.parser;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.tech_tec.qiitarian.QiitarianLog;
+import com.tech_tec.qiitarian.model.tags.Tag;
 import com.tech_tec.qiitarian.model.tags.Tags;
 
 public class TagsParser {
     
-    public Tags parse(JSONArray array) {
+    public Tags parse(JSONArray array) throws JSONException {
         QiitarianLog.d("start tags parse");
         
         Tags tags = new Tags();
+        
+        TagParser tagParser = new TagParser();
+        int size = array.length();
+        for (int i = 0; i < size; i++) {
+            JSONObject object = array.getJSONObject(i);
+            Tag tag = tagParser.parse(object);
+            tags.add(tag);
+        }
         
         QiitarianLog.d("end tags parse");
         return tags;
