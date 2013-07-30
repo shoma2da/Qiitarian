be2af1a92d1be3c377657fcc0844ee0cc5859fe5
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-30 21:16:00 +0900

    タグのリクエストにpageパラメータを追加

--- android/src/src/com/tech_tec/qiitarian/fragment/content/tags/http/TagsClient.java
+++ android/src/src/com/tech_tec/qiitarian/fragment/content/tags/http/TagsClient.java
@@ -3,20 +3,23 @@ package com.tech_tec.qiitarian.fragment.content.tags.http;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 
+import android.annotation.SuppressLint;
 import com.tech_tec.qiitarian.model.auth.UrlName;
 import com.tech_tec.qiitarian.model.http.ClientBase;
 
 public class TagsClient extends ClientBase {
     
     private UrlName mUrlName;
+    private int mPage;
     
-    public TagsClient(UrlName urlName) {
+    public TagsClient(UrlName urlName, int page) {
         mUrlName = urlName;
+        mPage = page;
     }
     
-    @Override
+    @Override @SuppressLint("DefaultLocale")
     protected HttpUriRequest createRequest() {
-        String url = String.format("/api/v1/users/%s/following_tags", mUrlName.toString());
+        String url = String.format("/api/v1/users/%s/following_tags?page=%d", mUrlName.toString(), mPage);
         return new HttpGet(url);
     }
     
