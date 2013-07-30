f34dc21c8ff968adbe7e2ac93a092f2bfafa5478
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-31 07:29:34 +0900

    タブによるリスト表示画面に遷移する実装

--- android/src/src/com/tech_tec/qiitarian/fragment/content/tags/GotoItemsByTagsOnItemClickListener.java
+++ android/src/src/com/tech_tec/qiitarian/fragment/content/tags/GotoItemsByTagsOnItemClickListener.java
@@ -1,10 +1,12 @@
 package com.tech_tec.qiitarian.fragment.content.tags;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 
+import com.tech_tec.qiitarian.activity.ItemsByTagActivity;
 import com.tech_tec.qiitarian.model.tags.Tag;
 
 public class GotoItemsByTagsOnItemClickListener implements OnItemClickListener {
@@ -19,7 +21,9 @@ public class GotoItemsByTagsOnItemClickListener implements OnItemClickListener {
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Tag tag = (Tag)parent.getItemAtPosition(position);
         
-        //Intentを送出する
+        Intent intent = new Intent(mActivity, ItemsByTagActivity.class);
+        //引数の設定
+        mActivity.startActivity(intent);
     }
 
 }
