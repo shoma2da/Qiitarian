2aa11de4d4a25ccd078118edb8e05c82c44e09e5
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-31 21:39:36 +0900

    アイテム一覧を表示する前の雛形の準備

--- android/src/src/com/tech_tec/qiitarian/activity/ItemsByTagActivity.java
+++ android/src/src/com/tech_tec/qiitarian/activity/ItemsByTagActivity.java
@@ -1,16 +1,24 @@
 package com.tech_tec.qiitarian.activity;
 
 import com.tech_tec.qiitarian.R;
+import com.tech_tec.qiitarian.fragment.list.CommandsAbstractFactory;
+import com.tech_tec.qiitarian.fragment.list.FactoryGettable;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class ItemsByTagActivity extends Activity {
+public class ItemsByTagActivity extends FragmentActivity implements FactoryGettable {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_by_tag);
+    }
+
+    @Override
+    public CommandsAbstractFactory getFactory() {
+        return null;
     }
     
 }
