f7a3d64f7cc4d6d3ed97b9fd68b6533451d45129
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-31 20:55:08 +0900

    インタフェースを準備

---
+++ android/src/src/com/tech_tec/qiitarian/fragment/list/FetchMoreItemsCommand.java
@@ -0,0 +1,6 @@
+package com.tech_tec.qiitarian.fragment.list;
+
+import android.widget.AbsListView.OnScrollListener;
+
+public interface FetchMoreItemsCommand extends OnScrollListener {
+}
---
+++ android/src/src/com/tech_tec/qiitarian/fragment/list/GetLatestItemsCommand.java
@@ -0,0 +1,6 @@
+package com.tech_tec.qiitarian.fragment.list;
+
+import com.markupartist.android.widget.PullToRefreshListView.OnRefreshListener;
+
+public interface GetLatestItemsCommand extends OnRefreshListener {
+}
