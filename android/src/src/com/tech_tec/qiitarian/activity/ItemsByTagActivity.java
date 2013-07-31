b28b6ed1e55130729f08bba8007cc00499d52201
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-07-31 21:39:53 +0900

    不要なimportの削除

--- android/src/src/com/tech_tec/qiitarian/activity/ItemsByTagActivity.java
+++ android/src/src/com/tech_tec/qiitarian/activity/ItemsByTagActivity.java
@@ -1,12 +1,11 @@
 package com.tech_tec.qiitarian.activity;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
 import com.tech_tec.qiitarian.R;
 import com.tech_tec.qiitarian.fragment.list.CommandsAbstractFactory;
 import com.tech_tec.qiitarian.fragment.list.FactoryGettable;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 
 public class ItemsByTagActivity extends FragmentActivity implements FactoryGettable {
     
