1003be87a20a5c517d4c6a354e6f75d38499269f
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-08-05 08:05:17 +0900

    ユーザ情報表示用のActivityを用意

---
+++ android/src/src/com/tech_tec/qiitarian/activity/user/UserActivity.java
@@ -0,0 +1,30 @@
+package com.tech_tec.qiitarian.activity.user;
+
+import java.io.Serializable;
+
+import com.tech_tec.qiitarian.fragment.user.UserUrlNameGettable;
+import com.tech_tec.qiitarian.model.common.UserUrlName;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class UserActivity extends FragmentActivity implements UserUrlNameGettable {
+    
+    private static final String KEY_USER_URLNAME = "user_urlname";
+    
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+    }
+    
+    @Override
+    public UserUrlName getUserUrlName() {
+        Serializable maybeUrlName = getIntent().getSerializableExtra(KEY_USER_URLNAME);
+        if (maybeUrlName instanceof UserUrlName == false) {
+            throw new RuntimeException("パラメータにはUserUrlNameをSerializableとして渡してください");
+        }
+        
+        return (UserUrlName)maybeUrlName;
+    }
+
+}
