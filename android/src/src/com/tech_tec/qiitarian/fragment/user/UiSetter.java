8be3fe78ffb4291e2020642b859515925082aa05
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-08-05 19:28:01 +0900

    不要なimportを削除

--- android/src/src/com/tech_tec/qiitarian/fragment/user/UiSetter.java
+++ android/src/src/com/tech_tec/qiitarian/fragment/user/UiSetter.java
@@ -1,15 +1,11 @@
 package com.tech_tec.qiitarian.fragment.user;
 
-import android.content.Context;
-import android.content.Intent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tech_tec.qiitarian.R;
-import com.tech_tec.qiitarian.activity.usersitem.UsersItemActivity;
 import com.tech_tec.qiitarian.model.user.User;
 import com.tech_tec.qiitarian.task.icon.FetchIconTask;
 
