de1356b2dd508b553b5c7bbd7b1e9701536789d8
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-08-05 20:14:59 +0900

    タイトルの表示

--- android/src/src/com/tech_tec/qiitarian/activity/usersitem/UsersItemActivity.java
+++ android/src/src/com/tech_tec/qiitarian/activity/usersitem/UsersItemActivity.java
@@ -2,6 +2,7 @@ package com.tech_tec.qiitarian.activity.usersitem;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
 
 import com.tech_tec.qiitarian.R;
 import com.tech_tec.qiitarian.fragment.list.CommandsAbstractFactory;
@@ -12,17 +13,23 @@ public class UsersItemActivity extends FragmentActivity implements FactoryGettab
     
     public static final String KEY_USER_URLNAME = "user_uslname";
     
+    private UserUrlName mUserUrlName;
+    
     @Override
     protected void onCreate(Bundle arg0) {
         super.onCreate(arg0);
         setContentView(R.layout.activity_list_usersitem);
         
+        String nameStr = getIntent().getStringExtra(KEY_USER_URLNAME);
+        mUserUrlName = new UserUrlName(nameStr);
+
+        TextView titleText = (TextView)findViewById(R.id.text_title);
+        titleText.setText(mUserUrlName.toString() + "さんの投稿一覧");
     }
 
     @Override
     public CommandsAbstractFactory getFactory() {
-        String nameStr = getIntent().getStringExtra(KEY_USER_URLNAME);
-        return new CommandsFactoryImpl(new UserUrlName(nameStr));
+        return new CommandsFactoryImpl(mUserUrlName);
     }
     
 }
