3539cbf1b8b955a2173c377778d60e401133f210
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-08-14 22:11:07 +0900

    ファイル名変更

--- android/src/src/com/tech_tec/qiitarian/activity/LoginActivity.java
+++ android/src/src/com/tech_tec/qiitarian/activity/LoginActivity.java
@@ -19,7 +19,7 @@ public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         
         initViews();
     }
