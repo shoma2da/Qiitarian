919e1c862646136ac22757bd3d17336d531f7ceb
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-08-17 15:00:01 +0900

    crittercismを導入

--- android/src/src/com/tech_tec/qiitarian/QiitarianApplication.java
+++ android/src/src/com/tech_tec/qiitarian/QiitarianApplication.java
@@ -2,5 +2,14 @@ package com.tech_tec.qiitarian;
 
 import android.app.Application;
 
+import com.crittercism.app.Crittercism;
+
 public class QiitarianApplication extends Application {
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Crittercism.init(getApplicationContext(), "520f0fa140020543f5000003");
+    }
+    
 }
