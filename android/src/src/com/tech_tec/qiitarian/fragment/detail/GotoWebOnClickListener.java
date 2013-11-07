32c78461e2328d9242812b2a202f80908b1bba1c
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-11-07 22:23:10 +0900

    ボタンのクリックイベントを仮実装

---
+++ android/src/src/com/tech_tec/qiitarian/fragment/detail/GotoWebOnClickListener.java
@@ -0,0 +1,14 @@
+package com.tech_tec.qiitarian.fragment.detail;
+
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+class GotoWebOnClickListener implements OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+        Log.d("Qiitarian", "go to web!");
+    }
+
+}
